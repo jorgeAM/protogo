22841df7b54cdfbf38a6a76260256360ac7dbf9a
Jorge Alfaro <jorge.alfmur@gmail.com>
2021-04-12 18:16:40 -0500

    generate code from proto file

---
+++ scripts/generate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if ! command go version >/dev/null 2>&1; then
+    echo "go is not installed"
+    # code 127 is when a command is not found
+    exit 127
+fi
+
+if ! command protoc --version >/dev/null 2>&1; then
+    echo "protoc not found ... please install protobuf compiler in yout machine"
+    # code 127 is when a command is not found
+    exit 127
+fi
+
+export GO111MODULE=on # Enable module mode
+
+PATH="$PATH:$(go env GOPATH)/bin"
+export PATH
+
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    addressbook.proto
